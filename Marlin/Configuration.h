9301edc8f83bd8c3b0588b8185773e33dc46d488
Victor Cazacov <cazacov.git@gmail.com>
2019-02-03 22:34:30 +0100

    Use A4988 for Z axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -533,7 +533,7 @@
  */
 #define X_DRIVER_TYPE  TMC2130
 #define Y_DRIVER_TYPE  TMC2130
-#define Z_DRIVER_TYPE  TMC2130
+#define Z_DRIVER_TYPE  A4988
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
 //#define Z2_DRIVER_TYPE A4988
@@ -831,7 +831,7 @@
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR false
 #define INVERT_Y_DIR true
-#define INVERT_Z_DIR false
+#define INVERT_Z_DIR true
 
 // @section extruder
 
